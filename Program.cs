2ec9f1b7e2ce1779b638e6d01a2d2eec9edcd2e4
Prasun Suwal <prasun.suwal@gmail.com>
2026-03-13 14:49:50 +0545

    Add GameDto and implement /games endpoint to retrieve game list

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using GameStore.Api.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new (2, "The Legend of Zelda: Ocarina of Time", "Adventure", 12.00M, new DateOnly(1998, 11, 21)),
+    new (3, "Super Mario Bros.", "Platformer", 40.24M, new DateOnly(1985, 9, 13))
+];
+
+// GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
